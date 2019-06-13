dc7d2030e3cb540275f8602390bc656c965f8696
IvanaaM <ivanamarin67@gmail.com>
2019-06-13 18:22:36 +0200

    Microservices, zuul, eureka

--- auth-service/src/main/java/com/ftn/micro1/controller/TestController.java
+++ auth-service/src/main/java/com/ftn/micro1/controller/TestController.java
@@ -1,15 +1,14 @@
 package com.ftn.micro1.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping(value="/test")
 public class TestController {
 	
-	@RequestMapping(value="/testString", method = RequestMethod.GET)
+	@RequestMapping(value="/method1", method = RequestMethod.GET)
 	public String Testing() {
 		
 		System.out.println("usao u metodu");
