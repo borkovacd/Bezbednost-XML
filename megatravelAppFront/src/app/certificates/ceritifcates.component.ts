f59e387e51b8f7efed4f9c5e7781e70e9a33d813
ilinkaKo <ilinkaakovacevic@gmail.com>
2019-06-16 19:59:57 +0200

    Small changes

--- megatravelAppFront/src/app/certificates/ceritifcates.component.ts
+++ megatravelAppFront/src/app/certificates/ceritifcates.component.ts
@@ -24,7 +24,7 @@ export class CeritifcatesComponent implements OnInit {
   visibleMessage: boolean;
   public form: FormGroup;
   public message: AbstractControl;
-  serialN: any;z
+  serialN: any;
 
   constructor(protected router: Router,
               private fb: FormBuilder, private data: SecurityService, private  userService: UserService,) {
