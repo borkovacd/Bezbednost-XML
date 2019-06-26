7dff43d658506cbfdca1d9eaa39f602a887a536d
Dragan Borkovac <borkovac.dragan@uns.ac.rs>
2019-06-26 11:23:30 +0200

    Zapoceta validacija na agentu

--- agentFront/src/app/log-in/log-in.component.ts
+++ agentFront/src/app/log-in/log-in.component.ts
@@ -31,20 +31,55 @@ export class LogInComponent implements OnInit {
 
     })
   }
+
+  validateLogData() {
+
+    let error = false;
+    let errorMessage = '';
+
+    /* PROVERA MEJLA */
+    /*const patternMail = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/;
+    if (!patternMail.test(this.email.value)) {
+      error = true;
+      errorMessage = 'Email adresa sadrzi nedozvoljene karaktere!';
+      return errorMessage;
+    }*/
+    return 'Ok';
+  }
+
+  escapeCharacters(value: string): string{
+    return value
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/\"/g, '&quot;')
+      /*.replace(/\'/g, '&#39;')*/
+      .replace(/\//g, '&#x2F;')
+      .replace('src', 'drc')
+      .replace(/\'/g, '&apos')
+
+  }
+
+
   confirmClick() {
-    const model = new LogInModel(
-      this.username.value,
-      this.password.value,
-    );
-    this.agentService.logIn(model).subscribe(data => {
-      if(data == null){
-        alert('Pogresna lozinka! Pokusajte ponovo.');
-      }else{
-        localStorage.setItem('agentId', data.accessToken);
-        this.router.navigateByUrl('/welcomepage');
 
-      }
+    let message = this.validateLogData();
 
-    })
+    if (message == "Ok") {
+      const model = new LogInModel(
+        this.escapeCharacters(this.username.value),
+        this.password.value,
+      );
+      this.agentService.logIn(model).subscribe(data => {
+        if (data == null) {
+          alert('Pogresna lozinka! Pokusajte ponovo.');
+        } else {
+          localStorage.setItem('agentId', data.accessToken);
+          this.router.navigateByUrl('/welcomepage');
+        }
+      })
+    } else {
+      alert(message);
+    }
   }
 }
